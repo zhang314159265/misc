b101d48e56088e25900fd138c0761aaef696ff44
zhang314159265 <decemberpig14@gmail.com>
2022-06-26 16:08:20 -0700

    embed base64 image in html
    By decoding the base64 string embedded in the html file, we can get a image file in
    format like PNG. Using PyPNG and PyQt5, we can view the PNG file with a simple python script.

---
+++ base64img/doc.md
@@ -0,0 +1,4 @@
+# Reference
+- [PNG - wikipedia](https://en.wikipedia.org/wiki/Portable_Network_Graphics): The doc explains clearly how chunks are defined in PNG overall. However it does not mention how data is compressed in the IDAT chunk.
+- [libpng - wikipedia](https://en.wikipedia.org/wiki/Libpng)
+- [PyPNG code examples](https://pypng.readthedocs.io/en/latest/ex.html)
---
+++ base64img/png_viewer.py
@@ -0,0 +1,42 @@
+# PNG Viewer:
+# Use libpng to extract pixel data from a png file and show the pixels using PyQt5
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QColor, QPainter
+import png
+
+class PNGViewer(QWidget):
+  def __init__(self, path):
+    super(PNGViewer, self).__init__()
+    self.setWindowTitle("PNG Viewer")
+    left = 10
+    top = 10
+
+    self.png = png.Reader(filename=path).read()
+    self.pad = 250
+
+    # first dimension is row/y rather than column/x
+    self.color_indices = list(self.png[2])
+    self.palette = self.png[3]["palette"]
+    self.width = self.png[0]
+    self.height = self.png[1]
+    self.setGeometry(left, top, self.width + 2 * self.pad, self.height + 2 * self.pad)
+    self.show()
+
+  def paintEvent(self, event):
+    super().paintEvent(event)
+
+    p = QPainter(self)
+    for x in range(self.width):
+      for y in range(self.height):
+        idx = self.color_indices[y][x]
+        color = self.palette[idx] # rgb or rgba
+        p.setPen(QColor(*color))
+        p.drawPoint(x + self.pad, y + self.pad)
+
+if __name__ == "__main__":
+  app = QApplication(sys.argv)
+  path = sys.argv[1] if len(sys.argv) >= 2 else "green-leaf.png"
+  ex = PNGViewer(path)
+  sys.exit(app.exec_())
